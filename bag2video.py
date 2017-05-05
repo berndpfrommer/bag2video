9b9977f147d8f3f23af505f475ea600ad258fe3a
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2017-05-05 09:09:51 -0400

    added multi-topic support

--- bag2video.py
+++ bag2video.py
@@ -25,15 +25,15 @@ def get_info(bag, topic=None, start_time=rospy.Time(0), stop_time=rospy.Time(sys
     times = []
 
     # read the first message to get the image size
-    msg = bag.read_messages(topics=topic).next()[1]
-    size = (msg.width, msg.height)
+    msg = bag.read_messages(topics=topic[0]).next()[1]
+    size = (msg.width * len(topic), msg.height)
 
     # now read the rest of the messages for the rates
-    iterator = bag.read_messages(topics=topic, start_time=start_time, end_time=stop_time)#, raw=True)
+    iterator = bag.read_messages(topics=topic[0], start_time=start_time, end_time=stop_time)#, raw=True)
     for _, msg, _ in iterator:
         time = msg.header.stamp
         times.append(time.to_sec())
-        size = (msg.width, msg.height)
+        size = (msg.width * len(topic), msg.height)
     diffs = 1/np.diff(times)
     return np.median(diffs), min(diffs), max(diffs), size, times
 
@@ -46,15 +46,28 @@ def write_frames(bag, writer, total, topic=None, nframes=repeat(1), start_time=r
     bridge = CvBridge()
     if viz:
         cv2.namedWindow('win')
-    count = 1
+    count = 0
     iterator = bag.read_messages(topics=topic, start_time=start_time, end_time=stop_time)
-    for (topic, msg, time), reps in izip(iterator, nframes):
-        print '\rWriting frame %s of %s at time %s' % (count, total, time),
-        img = np.asarray(bridge.imgmsg_to_cv(msg, 'bgr8'))
-        for rep in range(reps):
-            writer.write(img)
-        imshow('win', img)
-        count += 1
+    last_time = -1
+    frames = {}
+    ntopics  = len(topic)
+    for (topic, msg, time) in iterator:
+        tstamp = msg.header.stamp
+        img = np.asarray(bridge.imgmsg_to_cv2(msg, 'bgr8'))
+        if (tstamp != last_time): # new frame started
+            frames = {} # clear out incomplete frames
+            last_time = tstamp
+            count += 1
+        frames[topic] = img # remember frame for this topic
+        if len(frames) == ntopics and count < len(nframes): # have frames from all topics
+            # stack frames for all topics
+            wide_img = np.hstack([frames[t] for t in topics])
+            frames = {}
+            print '\rWriting frame %s of %s at time %s' % (count-1, total, tstamp)
+            for rep in range(nframes[count-1]):
+                writer.write(wide_img)
+            if viz:
+                imshow('win', wide_img)
 
 def imshow(win, img):
     cv2.imshow(win, img)
@@ -67,7 +80,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Extract and encode video from bag files.')
     parser.add_argument('--outfile', '-o', action='store', default=None,
                         help='Destination of the video file. Defaults to the location of the input file.')
-    parser.add_argument('--precision', '-p', action='store', default=10, type=int,
+    parser.add_argument('--precision', '-p', action='store', default=1, type=int,
                         help='Precision of variable framerate interpolation. Higher numbers\
                         match the actual framerater better, but result in larger files and slower conversion times.')
     parser.add_argument('--viz', '-v', action='store_true', help='Display frames in a GUI window.')
@@ -93,11 +106,12 @@ if __name__ == '__main__':
             outfile = os.path.join(*os.path.split(bagfile)[-1].split('.')[:-1]) + '.avi'
         bag = rosbag.Bag(bagfile, 'r')
         print 'Calculating video properties'
-        rate, minrate, maxrate, size, times = get_info(bag, args.topic, start_time=args.start, stop_time=args.end)
+        topics = args.topic.split(",")
+        rate, minrate, maxrate, size, times = get_info(bag, topics, start_time=args.start, stop_time=args.end)
         nframes = calc_n_frames(times, args.precision)
         # writer = cv2.VideoWriter(outfile, cv2.cv.CV_FOURCC(*'DIVX'), rate, size)
-        writer = cv2.VideoWriter(outfile, cv2.cv.CV_FOURCC(*'DIVX'), np.ceil(maxrate*args.precision), size)
+        writer = cv2.VideoWriter(outfile, cv2.VideoWriter_fourcc(*'DIVX'), np.ceil(maxrate*args.precision), size)
         print 'Writing video'
-        write_frames(bag, writer, len(times), topic=args.topic, nframes=nframes, start_time=args.start, stop_time=args.end, encoding=args.encoding)
+        write_frames(bag, writer, len(times), topic=topics, nframes=nframes, start_time=args.start, stop_time=args.end, viz=args.viz, encoding=args.encoding)
         writer.release()
         print '\n'
